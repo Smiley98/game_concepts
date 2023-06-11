9f2c2aaf19caaa611c0d5e2ec733fe6e7bc1a70c
Smiley98 <smileysofficialcinema@gmail.com>
2023-06-11 17:45:14 -0400

    Rotation in the direction of motion

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -10,15 +10,21 @@ int main(void)
     SetTargetFPS(60);
 
     Vector2 position{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
-    Vector2 velocity{};
+    Vector2 velocity{ 100.0f, 0.0f };
     Vector2 acceleration{};
+
+    Vector2 direction = Normalize(velocity);
+    float angularSpeed = 200.0f;
 
     while (!WindowShouldClose())
     {
         const float dt = GetFrameTime();
         velocity = velocity + acceleration * dt;
         position = position + velocity * dt + acceleration * dt * dt * 0.5f;
-        // "Semi-Implicit Euler Integration"
+
+        // Rotate at angular speed towards the direction of motion
+        const float frameRotation = angularSpeed * DEG2RAD * dt;
+        direction = RotateTowards(direction, Normalize(velocity), frameRotation);
 
         if (position.x >= SCREEN_WIDTH) position.x = 0.0f;
         else if (position.x <= 0.0f) position.x = SCREEN_WIDTH;
@@ -28,11 +34,13 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawCircleV(position, 25.0f, RED);
+        DrawLineV(position, position + direction * 100.0f, BLACK);
 
         rlImGuiBegin();
         ImGui::SliderFloat2("Position", &position.x, -500.0f, 500.0f);
         ImGui::SliderFloat2("Velocity", &velocity.x, -500.0f, 500.0f);
         ImGui::SliderFloat2("Acceleration", &acceleration.x, -500.0f, 500.0f);
+        ImGui::SliderFloat("Angular Speed", &angularSpeed, 0.0f, 360.0f);
         if (ImGui::Button("Reset"))
         {
             position = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
