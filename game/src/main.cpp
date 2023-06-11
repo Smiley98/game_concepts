1fce613d0e2f927d44127665f597fad72930b76c
Smiley98 <smileysofficialcinema@gmail.com>
2023-06-11 15:44:53 -0400

    Capsule collision

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,15 +1,123 @@
 #include "rlImGui.h"
+#include "Math.h"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+struct Circle
+{
+    Vector2 position{};
+    float radius;
+};
+
+struct Capsule
+{
+    Vector2 position{};
+    Vector2 direction{ 1.0f, 0.0f };
+    float radius;
+    float halfHeight;
+};
+
+void NearestCirclePoints(Capsule capsule1, Capsule capsule2, Vector2& nearest1, Vector2& nearest2)
+{
+    Vector2 top1 = capsule1.position + capsule1.direction * capsule1.halfHeight;
+    Vector2 top2 = capsule2.position + capsule2.direction * capsule2.halfHeight;
+    Vector2 bot1 = capsule1.position - capsule1.direction * capsule1.halfHeight;
+    Vector2 bot2 = capsule2.position - capsule2.direction * capsule2.halfHeight;
+
+    Vector2 lines[4]
+    {
+        top2 - top1,
+        bot2 - top1,
+        top2 - bot1,
+        bot2 - bot1,
+    };
+
+    // Determine nearest points between capsules
+    size_t min = 0;
+    for (size_t i = 1; i < 4; i++)
+    {
+        if (LengthSqr(lines[i]) < LengthSqr(lines[min]))
+            min = i;
+    }
+
+    // 1. Find whether 1's top or bot is closer to 2
+    // 2. Use that to find the point along 2 closest to 1
+    // 3. Use the point along 2 closest to 1 to find the point along 1 closest to 2
+    nearest1 = min < 2 ? top1 : bot1;
+    nearest2 = NearestPoint(bot2, top2, nearest1);
+    nearest1 = NearestPoint(bot1, top1, nearest2);
+}
+
+bool CircleCircle(Circle circle1, Circle circle2)
+{
+    return DistanceSqr(circle1.position, circle2.position) <=
+        powf(circle1.radius + circle2.radius, 2.0f);
+}
+
+bool CapsuleCapsule(Capsule capsule1, Capsule capsule2)
+{
+    Vector2 nearest1, nearest2;
+    NearestCirclePoints(capsule1, capsule2, nearest1, nearest2);
+    return CircleCircle({ nearest1, capsule1.radius }, { nearest2, capsule2.radius });
+}
+
+void DrawCapsule(Capsule capsule, Color color)
+{
+    Vector2 top = capsule.position + capsule.direction * capsule.halfHeight;
+    Vector2 bot = capsule.position - capsule.direction * capsule.halfHeight;
+    DrawCircleV(top, capsule.radius, color);
+    DrawCircleV(bot, capsule.radius, color);
+
+    Vector2 perp{ capsule.direction.y, -capsule.direction.x };
+    Vector2 leftStart = bot + perp * capsule.radius;
+    Vector2 leftEnd = leftStart + capsule.direction * capsule.halfHeight * 2.0f;
+    Vector2 rightStart = bot - perp * capsule.radius;
+    Vector2 rightEnd = rightStart + capsule.direction * capsule.halfHeight * 2.0f;
+    DrawLineEx(leftStart, leftEnd, 5.0f, color);
+    DrawLineEx(rightStart, rightEnd, 5.0f, color);
+
+    Rectangle rec{ capsule.position.x, capsule.position.y,
+        capsule.halfHeight * 2.0f, capsule.radius * 2.0f, };
+
+    DrawRectanglePro(rec, { capsule.halfHeight, capsule.radius, },
+        Angle(capsule.direction) * RAD2DEG, color);
+}
+
 int main(void)
 {
-    InitWindow(1280, 720, "Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
     SetTargetFPS(60);
+
+    float angularSpeed = 100.0f;    // 100 degrees per second
+    Capsule capsule1;
+    capsule1.radius = 25.0f;
+    capsule1.halfHeight = 50.0f;
+
+    Capsule capsule2;
+    capsule2.position = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    capsule2.direction = Direction(90.0f * DEG2RAD);
+    capsule2.radius = capsule1.radius;
+    capsule2.halfHeight = capsule1.halfHeight;
+
     while (!WindowShouldClose())
     {
+        capsule1.position = GetMousePosition();
+        const float dt = GetFrameTime();
+        if (IsKeyDown(KEY_E))
+            capsule1.direction = Rotate(capsule1.direction, angularSpeed * dt * DEG2RAD);
+        if (IsKeyDown(KEY_Q))
+            capsule1.direction = Rotate(capsule1.direction, -angularSpeed * dt * DEG2RAD);
+
+        bool collision = CapsuleCapsule(capsule1, capsule2);
+        Color color = collision ? RED : GREEN;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+        DrawCapsule(capsule1, color);
+        DrawCapsule(capsule2, color);
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
