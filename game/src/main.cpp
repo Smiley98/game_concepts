98367c96d97cad5fd422f334ea2ffcb244f94134
Smiley98 <smileysofficialcinema@gmail.com>
2023-06-12 19:42:45 -0400

    Pursue vs Evade

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -33,60 +33,72 @@ Vector2 Flee(Vector2 target, Vector2 seekerPosition, Vector2 seekerVelocity, flo
     return Normalize(seekerPosition - target) * speed - seekerVelocity;
 }
 
+Vector2 Pursue(Vector2 target, Vector2 targetVelocity, Vector2 seeker, Vector2 seekerVelocity, float speed)
+{
+    return Seek(target + targetVelocity * Distance(target, seeker) / Length(seekerVelocity), seeker, seekerVelocity, speed);
+}
+
+Vector2 Evade(Vector2 target, Vector2 targetVelocity, Vector2 seeker, Vector2 seekerVelocity, float speed)
+{
+    return Flee(target + targetVelocity * Distance(target, seeker) / Length(targetVelocity), seeker, seekerVelocity, speed);
+}
+
+void DrawBody(Rigidbody rb, Color color)
+{
+    DrawCircleV(rb.pos, 25.0f, color);
+    DrawLineV(rb.pos, rb.pos + rb.dir * 100.0f, BLACK);
+}
+
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
-    rlImGuiSetup(true);
     SetTargetFPS(60);
 
-    Rigidbody rb;
-    rb.pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    const Rectangle screen{ 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT };
+    const Vector2 center{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    Rigidbody rb, target;
+    rb.pos = target.pos = center;
+    rb.angularSpeed = target.angularSpeed = 200.0f * DEG2RAD;
     rb.vel = { 100.0f, 0.0f };
-
-    // User works in degrees, physics works in radians
-    float angularSpeed = 200.0f;
-    rb.angularSpeed = angularSpeed * DEG2RAD;
-    float linearSpeed = 500.0f;
-
-    // Interpolation parameter (0 = 0%, 1 = 100%, 0.25 = 25%, etc...)
-    float t = 0.0f;
+    // Must have an initial velocity otherwise Pursue() & Evade() divide by zero
 
     while (!WindowShouldClose())
     {
-        // Increase/decrease interpolation parameter with time and ensure range [0, 1]
+        // t approaches 1 as seeker approaches target, otherwise t = 0 if outside radius
+        const float evadeRadius = 500.0f;
+        const float distance = Distance(rb.pos, target.pos);
+        const float t = distance <= evadeRadius ? 1.0f - distance / evadeRadius : 0.0f;
         const float dt = GetFrameTime();
-        if (IsKeyDown(KEY_E)) t += dt;
-        if (IsKeyDown(KEY_Q)) t -= dt;
-        t = Clamp(t, 0.0f, 1.0f);
 
-        // Calculate both seek and flee forces, then interpolate between them
-        const Vector2 seek = Seek(GetMousePosition(), rb.pos, rb.vel, linearSpeed);
-        const Vector2 flee = Flee(GetMousePosition(), rb.pos, rb.vel, linearSpeed);
-        rb.acc = Lerp(seek, flee, t);
+        // Seek cursor so Pursue() & Evade() have a velocity to predict with
+        const float speed = 500.0f;
+        target.acc = Seek(GetMousePosition(), target.pos, target.vel, speed);
+        Update(target, dt);
+
+        // Calculate both pursue and evade forces, then interpolate between them
+        const Vector2 pursue = Pursue(target.pos, target.vel, rb.pos, rb.vel, speed);
+        const Vector2 evade = Evade(target.pos, target.vel, rb.pos, rb.vel, speed);
+        rb.acc = Lerp(pursue, evade, t);
         Update(rb, dt);
 
-        // Reset seeker to center if it travels off screen
-        if (rb.pos.x >= SCREEN_WIDTH || rb.pos.x <= 0.0f ||
-            rb.pos.y >= SCREEN_HEIGHT || rb.pos.y <= 0.0f)
-            rb.pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+        // Reset to center if off screen
+        rb.pos = CheckCollisionPointRec(rb.pos, screen) ? rb.pos : center;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(rb.pos, 25.0f, RED);
-        DrawLineV(rb.pos, rb.pos + rb.dir * 100.0f, BLACK);
-        DrawText("E/Q or drag slider to interpolate between seek and flee", 10, 10, 20, DARKGRAY);
-
-        rlImGuiBegin();
-        ImGui::SliderFloat("Interpolation", &t, 0.0f, 1.0f);
-        ImGui::SliderFloat("Linear Speed", &linearSpeed, 0.0f, 1000.0f);
-        if (ImGui::SliderFloat("Angular Speed", &angularSpeed, 0.0f, 360.0f))
-            rb.angularSpeed = angularSpeed * DEG2RAD;
-        rlImGuiEnd();
-
+        if (distance <= evadeRadius)
+        {
+            Color color = SKYBLUE;
+            color.a = 255.0f * t;
+            DrawCircleV(rb.pos, evadeRadius, color);
+        }
+        DrawBody(target, RED);
+        DrawBody(rb, BLUE);
+        DrawText("Blue circle will switch from pursuit to evasion the closer it gets to the red circle",
+            10, 10, 20, GRAY);
         EndDrawing();
     }
 
-    rlImGuiShutdown();
     CloseWindow();
     return 0;
 }
