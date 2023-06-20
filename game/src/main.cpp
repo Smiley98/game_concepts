654eb52c15cebb8df6858cbf943ca31baac4b565
Smiley98 <smileysofficialcinema@gmail.com>
2023-06-20 16:40:04 -0400

    Added slow radius and comparison vs naive arrive

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -44,6 +44,20 @@ Vector2 Arrive(Vector2 target, Vector2 seekerPosition, Vector2 seekerVelocity,
     return Seek(target, seekerPosition, seekerVelocity, speed) + deceleration;
 }
 
+Vector2 Arrive(Vector2 target, Vector2 seekerPosition, Vector2 seekerVelocity,
+    float speed, float slowRadius)
+{
+    Vector2 deceleration = Normalize(seekerVelocity) *
+        Decelerate(target, seekerPosition, seekerVelocity);
+
+    Vector2 seek = Seek(target, seekerPosition, seekerVelocity, speed);
+    Vector2 arrive = seek + deceleration;
+
+    // Interpolate between seek and arrive based on distance (t >= 1 if outside slow radius)
+    float t = Clamp(Distance(seekerPosition, target) / slowRadius, 0.0f, 1.0f);
+    return Lerp(arrive, seek, t);
+}
+
 // Given initial velocity (vi), final velocity (vf), and distance (d),
 // solve for acceleration (a) for which vf = 0 (arrival):
 // 
@@ -66,30 +80,36 @@ int main(void)
     rlImGuiSetup(true);
     SetTargetFPS(60);
 
-    Rigidbody rb;
-    rb.pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
-    rb.vel = { 100.0f, 0.0f };
+    Rigidbody rb1, rb2;
+    rb1.pos = rb2.pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    rb1.vel = rb2.vel = { 100.0f, 0.0f };
 
     // User works in degrees, physics works in radians
     float angularSpeed = 200.0f;
-    rb.angularSpeed = angularSpeed * DEG2RAD;
+    rb1.angularSpeed = rb2.angularSpeed = angularSpeed * DEG2RAD;
     float linearSpeed = 500.0f;
+    float slowRadius = 250.0f;
 
     while (!WindowShouldClose())
     {
         const float dt = GetFrameTime();
-        rb.acc = Arrive(GetMousePosition(), rb.pos, rb.vel, linearSpeed);
-        Update(rb, dt);
+        rb1.acc = Arrive(GetMousePosition(), rb1.pos, rb1.vel, linearSpeed);
+        rb2.acc = Arrive(GetMousePosition(), rb2.pos, rb2.vel, linearSpeed, slowRadius);
+        Update(rb1, dt);
+        Update(rb2, dt);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(rb.pos, 25.0f, RED);
-        DrawLineV(rb.pos, rb.pos + rb.dir * 100.0f, BLACK);
+        DrawCircleV(rb1.pos, 25.0f, { 230, 41, 55, 128 });
+        DrawCircleV(rb2.pos, 25.0f, { 230, 41, 55, 255 });
+        DrawLineV(rb1.pos, rb1.pos + rb1.dir * 100.0f, DARKGRAY);
+        DrawLineV(rb2.pos, rb2.pos + rb2.dir * 100.0f, BLACK);
 
         rlImGuiBegin();
+        ImGui::SliderFloat("Slow Radius", &slowRadius, 0.0f, 1000.0f);
         ImGui::SliderFloat("Linear Speed", &linearSpeed, 0.0f, 1000.0f);
         if (ImGui::SliderFloat("Angular Speed", &angularSpeed, 0.0f, 360.0f))
-            rb.angularSpeed = angularSpeed * DEG2RAD;
+            rb1.angularSpeed = rb2.angularSpeed = angularSpeed * DEG2RAD;
         rlImGuiEnd();
 
         EndDrawing();
