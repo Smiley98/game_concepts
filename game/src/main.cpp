c2b2f493f8ef7a09cb90cbdef3edd889ab7c9850
Smiley98 <smileysofficialcinema@gmail.com>
2023-06-25 20:56:24 -0400

    Decasteljau's algorithm visualized

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -16,6 +16,46 @@ Vector2 Decasteljau(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t)
     return Lerp(D, E, t);
 }
 
+// Decasteljau's Algorithm, but with the dots connected ;)
+void Visualize(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t)
+{
+    Vector2 A = Lerp(p0, p1, t);
+    Vector2 B = Lerp(p1, p2, t);
+    Vector2 C = Lerp(p2, p3, t);
+
+    Vector2 D = Lerp(A, B, t);
+    Vector2 E = Lerp(B, C, t);
+
+    // Draw end (p0 & p3) and control (p1 & p2) points in grey
+    DrawCircleV(p0, 10.0f, GRAY);
+    DrawCircleV(p1, 10.0f, GRAY);
+    DrawCircleV(p2, 10.0f, GRAY);
+    DrawCircleV(p3, 10.0f, GRAY);
+
+    // Draw lines between control & end points
+    DrawLineEx(p0, p1, 5.0f, ORANGE);
+    DrawLineEx(p1, p2, 5.0f, ORANGE);
+    DrawLineEx(p2, p3, 5.0f, ORANGE);
+
+    // Draw first layer of interpolation (3 inner points from 4 outer points)
+    DrawLineEx(A, B, 5.0f, GOLD);
+    DrawLineEx(B, C, 5.0f, GOLD);
+    DrawCircleV(A, 10.0f, GOLD);
+    DrawCircleV(B, 10.0f, GOLD);
+    DrawCircleV(C, 10.0f, GOLD);
+
+    // Draw second layer of interpolation (2 inner points from 3 outer points)
+    DrawLineEx(D, E, 5.0f, GREEN);
+    DrawCircleV(D, 10.0f, GREEN);
+    DrawCircleV(E, 10.0f, GREEN);
+
+    // Draw final point (1 resultant point given 2 initial points)
+    DrawCircleV(Lerp(D, E, t), 20.0f, BLUE);
+
+    // The following are identical:
+    // Lerp(D, E, t) == Decasteljau(p0, p1, p2, p3, t);
+}
+
 int main(void)
 {
     InitWindow(1280, 720, "Game");
@@ -36,18 +76,14 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawLineBezierCubic(p0, p3, p1, p2, 10.0f, RED);
-        DrawCircleV(p0, 10.0f, GRAY);
-        DrawCircleV(p1, 10.0f, GRAY);
-        DrawCircleV(p2, 10.0f, GRAY);
-        DrawCircleV(p3, 10.0f, GRAY);
-        DrawCircleV(Decasteljau(p0, p1, p2, p3, t), 20.0f, RED);
+        Visualize(p0, p1, p2, p3, t);
         DrawText("Drag the sliders to see how each point affects the curve", 10, 10, 20, GRAY);
 
         rlImGuiBegin();
         ImGui::SliderFloat2("End Point 1", &p0.x, 0.0f, SCREEN_WIDTH);
-        ImGui::SliderFloat2("End Point 2", &p3.x, 0.0f, SCREEN_WIDTH);
         ImGui::SliderFloat2("Control Point 1", &p1.x, 0.0f, SCREEN_WIDTH);
         ImGui::SliderFloat2("Control Point 2", &p2.x, 0.0f, SCREEN_WIDTH);
+        ImGui::SliderFloat2("End Point 2", &p3.x, 0.0f, SCREEN_WIDTH);
         rlImGuiEnd();
         EndDrawing();
     }
