90fae7284696793faed23521c641a0529ff8b916
Smiley98 <smileysofficialcinema@gmail.com>
2023-07-25 19:00:21 -0400

    Simplest Line of Sight completed

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,57 +1,79 @@
 #include "rlImGui.h"
 #include "Math.h"
+#include <algorithm>
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-bool InFoV(Vector2 viewerPosition, Vector2 viewerDirection, float viewDistance, float FoV/*radians*/, Vector2 targetPosition)
+bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
 {
-    // Don't bother with angle-check if target is far away from viewer
-    if (Distance(viewerPosition, targetPosition) > viewDistance) return false;
-    // Optimization: compare squared distances to avoid expensive square-root calculations
-    //if (DistanceSqr(viewerPosition, targetPosition) > viewDistance * viewDistance) return false;
-
-    // Vector from A to B = B - A
-    Vector2 targetDirection = Normalize(targetPosition - viewerPosition);
-
-    // Determine if the angle between viewer and target (dot product) is less than
-    // half the viewer's field of view (half to form a right-triangle)
-    float angle = acosf(Dot(viewerDirection, targetDirection));
-    return angle <= FoV * 0.5f;
-
-    // Optimization: Since arccos is the inverse of cos, we can flip the comparison to avoid an inverse trig function
-    //return Dot(viewerDirection, targetDirection) > cosf(FoV * 0.5f);
+    Vector2 nearest = ProjectPointLine(lineStart, lineEnd, circlePosition);
+    return DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
 }
 
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
-    rlImGuiSetup(true);
 
-    float FoV = 60.0f * DEG2RAD;  // field of view
-    float viewDistance = 500.0f;
-    Vector2 viewerPosition{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
-    Vector2 targetPosition{ SCREEN_WIDTH * 0.25f, SCREEN_HEIGHT * 0.5f };
+    Vector2 playerPosition{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    Vector2 playerDirection = Direction(0.0f);
+    float playerSpeed = 250.0f;
+    float playerRotationSpeed = 250.0f * DEG2RAD;
+    float playerLength = 250.0f;
+
+    Vector2 targetPosition{ SCREEN_WIDTH * 0.75f, SCREEN_HEIGHT * 0.25f };
+    float targetRadius = 25.0f;
+
+    Vector2 obstaclePosition = Lerp(playerPosition, targetPosition, 0.5f);
+    float obstacleRadius = 35.0f;
 
     while (!WindowShouldClose())
     {
-        Vector2 viewerDirection = Direction(GetTime());
-        bool targetVisible = InFoV(viewerPosition, viewerDirection, viewDistance, FoV, targetPosition);
+        const float dt = GetFrameTime();
+        const float playerSpeedDelta = playerSpeed * dt;
+        const float playerRotationDelta = playerRotationSpeed * dt;
+
+        if (IsKeyDown(KEY_W))
+        {
+            playerPosition = playerPosition + playerDirection * playerSpeedDelta;
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            playerPosition = playerPosition - playerDirection * playerSpeedDelta;
+        }
+        if (IsKeyDown(KEY_D))
+        {
+            playerDirection = Rotate(playerDirection, playerRotationDelta);
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            playerDirection = Rotate(playerDirection, -playerRotationDelta);
+        }
+
+        Vector2 playerEnd = playerPosition + playerDirection * playerLength;
+        bool targetCollision = LineCircle(playerPosition, playerEnd, targetPosition, targetRadius);
+        bool obstacleCollision = LineCircle(playerPosition, playerEnd, obstaclePosition, obstacleRadius);
+
+        // We're guaranteed to see the target if there's no obstacle collision,
+        // So only do distance comparison if there's an obstacle collision (and target collision)
+        // Calculating the exact points of intersection is expensive, so approximate distance using target & obstacle positions
+        bool targetVisible = targetCollision;
+        if (obstacleCollision && targetCollision)
+        {
+            float targetDistance = Distance(playerPosition, targetPosition);
+            float obstacleDistance = Distance(playerPosition, obstaclePosition);
+            targetVisible = targetDistance < obstacleDistance;
+        }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(viewerPosition, 25.0f, BLUE);
-        DrawLineV(viewerPosition, viewerPosition + Rotate(viewerDirection, FoV *  0.5f) * viewDistance, BLUE);
-        DrawLineV(viewerPosition, viewerPosition + Rotate(viewerDirection, FoV * -0.5f) * viewDistance, BLUE);
-        DrawCircleV(targetPosition, 25.0f, targetVisible ? GREEN : RED);
-        rlImGuiBegin();
-        ImGui::SliderAngle("Field of View", &FoV, 10.0f, 100.0f);
-        ImGui::SliderFloat("View Distance", &viewDistance, 100.0f, 750.0f);
-        rlImGuiEnd();
+        DrawLineV(playerPosition, playerEnd, BLUE);
+        DrawCircleV(playerPosition, 25.0f, BLUE);
+        DrawCircleV(targetPosition, targetRadius, targetVisible ? RED : GREEN);
+        DrawCircleV(obstaclePosition, obstacleRadius, GRAY);
         EndDrawing();
     }
 
-    rlImGuiShutdown();
     CloseWindow();
     return 0;
 }
