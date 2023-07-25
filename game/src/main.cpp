e2c62af985e909ea65ba33509c74d0e54bdf0c33
Smiley98 <smileysofficialcinema@gmail.com>
2023-07-25 16:58:08 -0400

    Added rotating viewer

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,13 +1,27 @@
 #include "rlImGui.h"
+#include "Math.h"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
 int main(void)
 {
-    InitWindow(1280, 720, "Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
     SetTargetFPS(60);
+
+    float FoV = 60.0f * DEG2RAD;  // field of view
+    float viewDistance = 250.0f;
+    Vector2 viewerPosition{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    Vector2 targetPosition{ SCREEN_WIDTH * 0.75f, SCREEN_HEIGHT * 0.25f };
+
     while (!WindowShouldClose())
     {
+        Vector2 viewerDirection = Direction(GetTime());
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+        DrawCircleV(viewerPosition, 25.0f, BLUE);
+        DrawLineV(viewerPosition, viewerPosition + Rotate(viewerDirection, FoV *  0.5f) * viewDistance, BLUE);
+        DrawLineV(viewerPosition, viewerPosition + Rotate(viewerDirection, FoV * -0.5f) * viewDistance, BLUE);
         EndDrawing();
     }
     CloseWindow();
