4faaf1ba617a73c9e4c681e600907af176465f15
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-03 19:57:05 -0400

    Mouse input

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,13 +1,29 @@
 #include "rlImGui.h"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
 int main(void)
 {
-    InitWindow(1280, 720, "Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
     SetTargetFPS(60);
+
+    bool toggleColour = false;
+    Rectangle screen{ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+    Vector2 position{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
     while (!WindowShouldClose())
     {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+            toggleColour = !toggleColour;
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+            position = GetMousePosition();
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+        DrawRectangleRec(screen, toggleColour ? GREEN : RED);
+        DrawCircleV(position, 25.0f, BLUE);
+        DrawText("Hold down the left mouse button to drag around the blue circle.", 10, 10, 20, DARKGRAY);
+        DrawText("Left-click to change between a green and red background.", 10, 30, 20, DARKGRAY);
         EndDrawing();
     }
     CloseWindow();
