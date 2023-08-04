9bbbdc1022ba2eaa39a339067cd4f8db5820fa99
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-04 17:06:01 -0400

    Bounded intersections at extremes using projection, and added analytical solution (which is more percise)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,5 +1,6 @@
 #include "rlImGui.h"
 #include "Math.h"
+#include <cstdio>
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
@@ -18,7 +19,17 @@
 // 
 // Solve for B by translating from projection by b units in the direction of the line:
 // B = A + D * b
-bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Vector2* poi1, Vector2* poi2)
+//
+// Finally, we determine which points of intersection actually lie on the line
+// by checking if their projections are within the normalized range (0 >= t <= 1)
+struct Hit
+{
+    Vector2 poi{};      // point of intersection
+    bool hit = false;   // whether poi is valid
+};
+
+// Geometrical solution (solve the triangle as mentioned above)
+bool LineCircle1(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Hit& hit1, Hit& hit2)
 {
     Vector2 proj = ProjectPointLine(lineStart, lineEnd, circlePosition);
     float c2 = circleRadius * circleRadius;
@@ -27,12 +38,46 @@ bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, floa
     {
         float a2 = DistanceSqr(proj, circlePosition);
         float b = sqrtf(c2 - a2);
-        Vector2 direction = Normalize(lineEnd - lineStart);
-        *poi1 = proj - direction * b;
-        *poi2 = proj + direction * b;
-        // TODO -- use t-value of projection to determine whether pois are outside of line
+
+        Vector2 line = lineEnd - lineStart;
+        Vector2 lineDirection = Normalize(line);
+        float lineLength2 = LengthSqr(line);
+        
+        Vector2 poi1 = proj - lineDirection * b;
+        Vector2 poi2 = proj + lineDirection * b;
+        float t1 = Dot(poi1 - lineStart, line) / lineLength2;
+        float t2 = Dot(poi2 - lineStart, line) / lineLength2;
+
+        hit1.poi = poi1;
+        hit2.poi = poi2;
+        hit1.hit = t1 >= 0.0f && t1 <= 1.0f;
+        hit2.hit = t2 >= 0.0f && t2 <= 1.0f;
     }
     return collision;
+}
+
+// Analytical solution (solves a quadratic -- similar to the geometric solution, but with calculus) 
+bool LineCircle2(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Hit& hit1, Hit& hit2)
+{
+    Vector2 dc = lineStart - circlePosition;
+    Vector2 dx = lineEnd - lineStart;
+    float a = LengthSqr(dx);
+    float b = Dot(dx, dc) * 2.0f;
+    float c = LengthSqr(dc) - circleRadius * circleRadius;
+    float det = b * b - 4.0f * a * c;
+
+    if (a <= FLT_EPSILON || det < 0.0f) return false;
+
+    det = sqrtf(det);
+    float t1 = (-b - det) / (2.0f * a);
+    float t2 = (-b + det) / (2.0f * a);
+
+    hit1.poi = lineStart + dx * t1;
+    hit2.poi = lineStart + dx * t2;
+    hit1.hit = t1 >= 0.0f && t1 <= 1.0f;
+    hit2.hit = t2 >= 0.0f && t2 <= 1.0f;
+
+    return true;
 }
 
 int main(void)
@@ -60,18 +105,22 @@ int main(void)
         Vector2 lineStart = center + direction * length * 0.5f;
         Vector2 lineEnd = center + direction * length * -0.5f;
 
-        Vector2 poi1{}, poi2{};
-        bool collision = LineCircle(lineStart, lineEnd, circlePosition, radius, &poi1, &poi2);
+        Hit hit1, hit2;
+        bool collision = LineCircle2(lineStart, lineEnd, circlePosition, radius, hit1, hit2);
         Color color = collision ? RED : GREEN;
+        //bool collision = LineCircle1(lineStart, lineEnd, circlePosition, radius, hit1, hit2);
+        // This gets a little glitchy around the edges, so the analytical solution is prefered.
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawLineEx(lineStart, lineEnd, thickness, color);
+        DrawLineEx(lineStart, lineEnd, 1.0f, color);
         DrawCircleV(circlePosition, radius, color);
         if (collision)
         {
-            DrawCircleV(poi1, 5.0f, ORANGE);
-            DrawCircleV(poi2, 5.0f, ORANGE);
+            if (hit1.hit)
+                DrawCircleV(hit1.poi, 5.0f, ORANGE);
+            if (hit2.hit)
+                DrawCircleV(hit2.poi, 5.0f, ORANGE);
         }
         DrawText("Move the circle with your mouse.", 10, 10, 20, GRAY);
         DrawText("Hold E & Q to rotate the line clockwise/counter-clockwise.", 10, 30, 20, GRAY);
