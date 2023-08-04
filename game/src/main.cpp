75fbd6bfd99590151a63f2a0798808976bb12a12
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-04 19:13:56 -0400

    Line circle resolution

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,80 +4,20 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-// We can solve a right-triangle to determine the points of intersection between the line and circle:
-// let A = projection of line onto circle
-// let C = circle center
-// let a = Length(C - A)
-// let c = circle radius
-// let B = point of intersection ("poi")
-// let b = Length(B - A)
-// let D = direction of line
-// 
-// b and B are unknown.
-// Solve for b using Pythagorean theorem
-// b = sqrt(c^2 - a^2)
-// 
-// Solve for B by translating from projection by b units in the direction of the line:
-// B = A + D * b
-//
-// Finally, we determine which points of intersection actually lie on the line
-// by checking if their projections are within the normalized range (0 >= t <= 1)
-struct Hit
+// MTV (minimum translation vector) resolves circle from line (only written to on-collision)
+bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Vector2* mtv = nullptr)
 {
-    Vector2 poi{};      // point of intersection
-    bool hit = false;   // whether poi is valid
-};
-
-// Geometrical solution (solve the triangle as mentioned above)
-bool LineCircle1(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Hit& hit1, Hit& hit2)
-{
-    Vector2 proj = ProjectPointLine(lineStart, lineEnd, circlePosition);
-    float c2 = circleRadius * circleRadius;
-    bool collision = DistanceSqr(proj, circlePosition) < c2;
-    if (collision)
+    Vector2 nearest = ProjectPointLine(lineStart, lineEnd, circlePosition);
+    bool collision = DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
+    if (collision && mtv != nullptr)
     {
-        float a2 = DistanceSqr(proj, circlePosition);
-        float b = sqrtf(c2 - a2);
-
-        Vector2 line = lineEnd - lineStart;
-        Vector2 lineDirection = Normalize(line);
-        float lineLength2 = LengthSqr(line);
-        
-        Vector2 poi1 = proj - lineDirection * b;
-        Vector2 poi2 = proj + lineDirection * b;
-        float t1 = Dot(poi1 - lineStart, line) / lineLength2;
-        float t2 = Dot(poi2 - lineStart, line) / lineLength2;
-
-        hit1.poi = poi1;
-        hit2.poi = poi2;
-        hit1.hit = t1 >= 0.0f && t1 <= 1.0f;
-        hit2.hit = t2 >= 0.0f && t2 <= 1.0f;
+        // Compare distance from projection to center vs radius to determine penetration depth,
+        // then translate by penetration depth in the direction from circle to projection!
+        Vector2 resolutionDirection = Normalize(circlePosition - nearest);
+        float resolutionLength = circleRadius - Distance(nearest, circlePosition);
+        *mtv = resolutionDirection * resolutionLength;
     }
     return collision;
-}
-
-// Analytical solution (solves a quadratic -- similar to the geometric solution, but with calculus) 
-bool LineCircle2(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Hit& hit1, Hit& hit2)
-{
-    Vector2 dc = lineStart - circlePosition;
-    Vector2 dx = lineEnd - lineStart;
-    float a = LengthSqr(dx);
-    float b = Dot(dx, dc) * 2.0f;
-    float c = LengthSqr(dc) - circleRadius * circleRadius;
-    float det = b * b - 4.0f * a * c;
-
-    if (a <= FLT_EPSILON || det < 0.0f) return false;
-
-    det = sqrtf(det);
-    float t1 = (-b - det) / (2.0f * a);
-    float t2 = (-b + det) / (2.0f * a);
-
-    hit1.poi = lineStart + dx * t1;
-    hit2.poi = lineStart + dx * t2;
-    hit1.hit = t1 >= 0.0f && t1 <= 1.0f;
-    hit2.hit = t2 >= 0.0f && t2 <= 1.0f;
-
-    return hit1.hit || hit2.hit;
 }
 
 int main(void)
@@ -85,7 +25,7 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
     SetTargetFPS(60);
 
-    Vector2 screenCenter{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    Vector2 lineCenter{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
     Vector2 lineDirection{ 1.0f, 0.0f };
     float lineLength = 500.0f;
 
@@ -101,31 +41,28 @@ int main(void)
         if (IsKeyDown(KEY_Q))
             lineDirection = Rotate(lineDirection, -rotation);
 
-        Vector2 lineStart = screenCenter + lineDirection * lineLength * 0.5f;
-        Vector2 lineEnd = screenCenter + lineDirection * lineLength * -0.5f;
+        Vector2 lineStart = lineCenter + lineDirection * lineLength * 0.5f;
+        Vector2 lineEnd = lineCenter + lineDirection * lineLength * -0.5f;
+        Vector2 mtv;
 
-        Hit hit1, hit2;
-        //bool collision = LineCircle1(lineStart, lineEnd, circlePosition, circleRadius, hit1, hit2);
-        // Geometric solution is inaccurate near edges, so the analytical solution is prefered.
-        bool collision = LineCircle2(lineStart, lineEnd, circlePosition, circleRadius, hit1, hit2);
-        Color color = collision ? RED : GREEN;
+        // More useful to resolve line from circle than circle from line
+        // (since the circle will just flip from one side to the other of the line).
+        //if (LineCircle(lineStart, lineEnd, circlePosition, circleRadius, &mtv))
+        //    circlePosition = circlePosition + mtv;
+        if (LineCircle(lineStart, lineEnd, circlePosition, circleRadius, &mtv))
+            lineCenter = lineCenter - mtv;
+        
+        // Determine colour after collision-resolution
+        Color color = LineCircle(lineStart, lineEnd, circlePosition, circleRadius) ? RED : GREEN;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawLineEx(lineStart, lineEnd, 5.0f, color);
         DrawCircleV(circlePosition, circleRadius, color);
         DrawCircleV(ProjectPointLine(lineStart, lineEnd, circlePosition), 5.0f, GRAY);
-        if (collision)
-        {
-            if (hit1.hit)
-                DrawCircleV(hit1.poi, 5.0f, ORANGE);
-            if (hit2.hit)
-                DrawCircleV(hit2.poi, 5.0f, ORANGE);
-        }
         DrawText("Move the circle with your mouse.", 10, 10, 20, GRAY);
         DrawText("Hold E & Q to rotate the line clockwise/counter-clockwise.", 10, 30, 20, GRAY);
-        DrawText("Circle-projection in grey, points of intersection in orange.", 10, 50, 20, GRAY);
-
+        DrawText("Circle-projection in grey.", 10, 50, 20, GRAY);
         EndDrawing();
     }
 
