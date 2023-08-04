41e7e1424cda6bb442310fd8f8841f22a7d2bc7d
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-03 20:02:27 -0400

    Key input

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -12,18 +12,30 @@ int main(void)
     Vector2 position{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
     while (!WindowShouldClose())
     {
-        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+        // Use time-based instead of frame-based movement to remain framerate-agnostic!
+        float speed = 500.0f * GetFrameTime();
+
+        if (IsKeyPressed(KEY_SPACE))
             toggleColour = !toggleColour;
 
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            position = GetMousePosition();
+        if (IsKeyDown(KEY_W))
+            position.y -= speed;
+
+        if (IsKeyDown(KEY_S))
+            position.y += speed;
+
+        if (IsKeyDown(KEY_A))
+            position.x -= speed;
+
+        if (IsKeyDown(KEY_D))
+            position.x += speed;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawRectangleRec(screen, toggleColour ? GREEN : RED);
         DrawCircleV(position, 25.0f, BLUE);
-        DrawText("Hold down the left mouse button to drag around the blue circle.", 10, 10, 20, DARKGRAY);
-        DrawText("Left-click to change between a green and red background.", 10, 30, 20, DARKGRAY);
+        DrawText("Hold WASD to move the blue circle.", 10, 10, 20, DARKGRAY);
+        DrawText("Press SPACE to change between a green and red background.", 10, 30, 20, DARKGRAY);
         EndDrawing();
     }
     CloseWindow();
