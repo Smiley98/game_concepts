a47cb609360dbdcde8f2415d38d8856aa186167a
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-04 16:31:34 -0400

    Line circle intersection

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,12 +3,36 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-// Project circle-position onto line segment, then perform a point-circle collision check with the projected point!
-bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
+// We can solve a right-triangle to determine the points of intersection between the line and circle:
+// let A = projection of line onto circle
+// let C = circle center
+// let a = Length(C - A)
+// let c = circle radius
+// let B = point of intersection ("poi")
+// let b = Length(B - A)
+// let D = direction of line
+// 
+// b and B are unknown.
+// Solve for b using Pythagorean theorem
+// b = sqrt(c^2 - a^2)
+// 
+// Solve for B by translating from projection by b units in the direction of the line:
+// B = A + D * b
+bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Vector2* poi1, Vector2* poi2)
 {
-    Vector2 nearest = ProjectPointLine(lineStart, lineEnd, circlePosition);
-    return DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
-    // More optimal to compare squared distance instead of using a more expensive square-root calculation!
+    Vector2 proj = ProjectPointLine(lineStart, lineEnd, circlePosition);
+    float c2 = circleRadius * circleRadius;
+    bool collision = DistanceSqr(proj, circlePosition) < c2;
+    if (collision)
+    {
+        float a2 = DistanceSqr(proj, circlePosition);
+        float b = sqrtf(c2 - a2);
+        Vector2 direction = Normalize(lineEnd - lineStart);
+        *poi1 = proj - direction * b;
+        *poi2 = proj + direction * b;
+        // TODO -- use t-value of projection to determine whether pois are outside of line
+    }
+    return collision;
 }
 
 int main(void)
@@ -20,7 +44,9 @@ int main(void)
     Vector2 direction{ 1.0f, 0.0f };
     float length = 500.0f;
     float thickness = 5.0f;
+    float radius = 20.0f;
 
+    HideCursor();
     while (!WindowShouldClose())
     {
         Vector2 circlePosition = GetMousePosition();
@@ -34,17 +60,19 @@ int main(void)
         Vector2 lineStart = center + direction * length * 0.5f;
         Vector2 lineEnd = center + direction * length * -0.5f;
 
-        // Project circle-position onto the line segment
-        Vector2 projection = ProjectPointLine(lineStart, lineEnd, circlePosition);
-
-        bool collision = LineCircle(lineStart, lineEnd, circlePosition, 20.0f);
+        Vector2 poi1{}, poi2{};
+        bool collision = LineCircle(lineStart, lineEnd, circlePosition, radius, &poi1, &poi2);
         Color color = collision ? RED : GREEN;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(circlePosition, 20.0f, color);
-        DrawCircleV(projection, 10.0f, GRAY);
         DrawLineEx(lineStart, lineEnd, thickness, color);
+        DrawCircleV(circlePosition, radius, color);
+        if (collision)
+        {
+            DrawCircleV(poi1, 5.0f, ORANGE);
+            DrawCircleV(poi2, 5.0f, ORANGE);
+        }
         DrawText("Move the circle with your mouse.", 10, 10, 20, GRAY);
         DrawText("Hold E & Q to rotate the line clockwise/counter-clockwise.", 10, 30, 20, GRAY);
         EndDrawing();
