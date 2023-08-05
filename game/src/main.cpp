c8c8090ac7ca5097aa4dfbc3d56251a44b0b939f
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-04 22:43:54 -0400

    Line capsule resolution

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,20 +4,55 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-// MTV (minimum translation vector) resolves circle from line (only written to on-collision)
-bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius, Vector2* mtv = nullptr)
+// Capsules can be thought of as two circles with a rectangle in-between them
+struct Capsule
 {
-    Vector2 nearest = ProjectPointLine(lineStart, lineEnd, circlePosition);
-    bool collision = DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
+    Vector2 position{};
+    Vector2 direction{ 1.0f, 0.0f };
+    float radius;
+    float halfLength;
+    // Distance between position (center) and top/bot "circle"
+};
+
+// MTV (minimum translation vector) resolves circle from point (only written to on-collision)
+bool PointCircle(Vector2 point, Vector2 circlePosition, float circleRadius, Vector2* mtv = nullptr)
+{
+    bool collision = DistanceSqr(point, circlePosition) <= circleRadius * circleRadius;
     if (collision && mtv != nullptr)
     {
         // Compare distance from projection to center vs radius to determine penetration depth,
         // then translate by penetration depth in the direction from circle to projection!
-        Vector2 resolutionDirection = Normalize(circlePosition - nearest);
-        float resolutionLength = circleRadius - Distance(nearest, circlePosition);
+        Vector2 resolutionDirection = Normalize(circlePosition - point);
+        float resolutionLength = circleRadius - Distance(point, circlePosition);
         *mtv = resolutionDirection * resolutionLength;
     }
     return collision;
+}
+
+// Project capsule onto line and line onto capsule. Resolve as point-circle collision.
+bool LineCapsule(Vector2 lineStart, Vector2 lineEnd, Capsule capsule, Vector2* mtv = nullptr)
+{
+    Vector2 top = capsule.position + capsule.direction * capsule.halfLength;
+    Vector2 bot = capsule.position - capsule.direction * capsule.halfLength;
+    Vector2 projTop = ProjectPointLine(lineStart, lineEnd, top);
+    Vector2 projBot = ProjectPointLine(lineStart, lineEnd, bot);
+    Vector2 projCapsuleLine = DistanceSqr(top, projTop) < DistanceSqr(bot, projBot) ? projTop : projBot;
+    Vector2 projLineCapsule = ProjectPointLine(top, bot, projCapsuleLine);
+    return PointCircle(projCapsuleLine, projLineCapsule, capsule.radius, mtv);
+}
+
+void DrawCapsule(Capsule capsule, Color color)
+{
+    Vector2 top = capsule.position + capsule.direction * capsule.halfLength;
+    Vector2 bot = capsule.position - capsule.direction * capsule.halfLength;
+    DrawCircleV(top, capsule.radius, color);
+    DrawCircleV(bot, capsule.radius, color);
+
+    // Half-Length is along x because objects have the identity direction of [1, 0]
+    Rectangle rec{ capsule.position.x, capsule.position.y,
+        capsule.halfLength * 2.0f, capsule.radius * 2.0f };
+    DrawRectanglePro(rec, { capsule.halfLength, capsule.radius },
+        Angle(capsule.direction) * RAD2DEG, color);
 }
 
 int main(void)
@@ -29,40 +64,54 @@ int main(void)
     Vector2 lineDirection{ 1.0f, 0.0f };
     float lineLength = 500.0f;
 
+    Capsule capsule;
+    capsule.halfLength = 50.0f;
+    capsule.radius = 20.0f;
+
     HideCursor();
     while (!WindowShouldClose())
     {
-        Vector2 circlePosition = GetMousePosition();
-        float circleRadius = 20.0f;
-
         float rotation = 250.0f * DEG2RAD * GetFrameTime();
+        capsule.position = GetMousePosition();
         if (IsKeyDown(KEY_E))
             lineDirection = Rotate(lineDirection, rotation);
         if (IsKeyDown(KEY_Q))
             lineDirection = Rotate(lineDirection, -rotation);
+        if (IsKeyDown(KEY_D))
+            capsule.direction = Rotate(capsule.direction, rotation);
+        if (IsKeyDown(KEY_A))
+            capsule.direction = Rotate(capsule.direction, -rotation);
 
         Vector2 lineStart = lineCenter + lineDirection * lineLength * 0.5f;
         Vector2 lineEnd = lineCenter + lineDirection * lineLength * -0.5f;
         Vector2 mtv;
 
-        // More useful to resolve line from circle than circle from line
-        // (since the circle will just flip from one side to the other of the line).
-        //if (LineCircle(lineStart, lineEnd, circlePosition, circleRadius, &mtv))
-        //    circlePosition = circlePosition + mtv;
-        if (LineCircle(lineStart, lineEnd, circlePosition, circleRadius, &mtv))
+        // Resolve line from capsule
+        if (LineCapsule(lineStart, lineEnd, capsule, &mtv))
             lineCenter = lineCenter - mtv;
         
         // Determine colour after collision-resolution
-        Color color = LineCircle(lineStart, lineEnd, circlePosition, circleRadius) ? RED : GREEN;
+        Color color = LineCapsule(lineStart, lineEnd, capsule) ? RED : GREEN;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawLineEx(lineStart, lineEnd, 5.0f, color);
-        DrawCircleV(circlePosition, circleRadius, color);
-        DrawCircleV(ProjectPointLine(lineStart, lineEnd, circlePosition), 5.0f, GRAY);
-        DrawText("Move the circle with your mouse.", 10, 10, 20, GRAY);
-        DrawText("Hold E & Q to rotate the line clockwise/counter-clockwise.", 10, 30, 20, GRAY);
-        DrawText("Circle-projection in grey.", 10, 50, 20, GRAY);
+        DrawCapsule(capsule, color);
+
+        // Render projection math
+        Vector2 top = capsule.position + capsule.direction * capsule.halfLength;
+        Vector2 bot = capsule.position - capsule.direction * capsule.halfLength;
+        Vector2 projTop = ProjectPointLine(lineStart, lineEnd, top);
+        Vector2 projBot = ProjectPointLine(lineStart, lineEnd, bot);
+        Vector2 projCapsuleLine = DistanceSqr(top, projTop) < DistanceSqr(bot, projBot) ? projTop : projBot;
+        Vector2 projLineCapsule = ProjectPointLine(top, bot, projCapsuleLine);
+        DrawCircleV(projCapsuleLine, 5.0f, BLUE);
+        DrawCircleV(projLineCapsule, 5.0f, PURPLE);
+
+        DrawText("Hold A & D to rotate the capsule clockwise/counter-clockwise.", 10, 10, 20, BLUE);
+        DrawText("Hold E & Q to rotate the line clockwise/counter-clockwise.", 10, 30, 20, PURPLE);
+        DrawText("Projection of capsule onto line in blue", 10, 50, 20, BLUE);
+        DrawText("Projection of line onto capsule in purple", 10, 70, 20, PURPLE);
         EndDrawing();
     }
 
