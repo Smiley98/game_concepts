e7f1a1152347c6324afbaa999efaead2b0544805
Smiley98 <smileysofficialcinema@gmail.com>
2023-08-05 13:53:57 -0400

    Transformed points (hexagon)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,15 +1,69 @@
 #include "rlImGui.h"
+#include "Math.h"
+#include <vector>
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+using Points = std::vector<Vector2>;
+
+struct Transform2
+{
+    Vector2 translation{};
+    float rotation = 0.0f;
+    float scale = 1.0f;
+};
+
+void Apply(const Transform2& transform, Points& points)
+{
+    for (Vector2& point : points)
+        point = Rotate((point * transform.scale), transform.rotation) + transform.translation;
+}
+
 int main(void)
 {
-    InitWindow(1280, 720, "Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
     SetTargetFPS(60);
+
+    // Define our polygon relative to the origin [0, 0], then translate/rotate/scale accordingly!
+    Points hexagon
+    {
+        { -0.5f, -0.33f },  // top left
+        {  0.0f, -1.66f },  // top middle
+        {  0.5f, -0.33f },  // top right
+        {  0.5f,  0.33f },  // bottom left
+        {  0.0f,  1.66f },  // bottom middle
+        { -0.5f,  0.33f },  // bottom right
+        { -0.5f, -0.33f }   // connect end to start
+        // (necessary for existing collsion & rendering functions)
+    };
+
+    Transform2 transform;
+    transform.translation = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    transform.scale = 100.0f;
+
+    float angularSpeed = 250.0f * DEG2RAD;
     while (!WindowShouldClose())
     {
+        float rotationDelta = angularSpeed * GetFrameTime();
+        if (IsKeyDown(KEY_Q))
+            transform.rotation -= rotationDelta;
+        if (IsKeyDown(KEY_E))
+            transform.rotation += rotationDelta;
+
+        Points points = hexagon;
+        Apply(transform, points);
+
+        Vector2 mouse = GetMousePosition();
+        bool collision = CheckCollisionPointPoly(mouse, points.data(), points.size());
+        Color color = collision ? RED : GREEN;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+        DrawCircleV(mouse, 5.0f, DARKGRAY);
+        DrawLineStrip(points.data(), points.size(), color);
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
