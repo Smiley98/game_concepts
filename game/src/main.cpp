b02220dad1c344c239df13700d808627942648c7
Smiley98 <smileysofficialcinema@gmail.com>
2023-11-16 09:22:30 -0500

    Circle resolution visualized

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,9 +3,17 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-bool CircleCircle(Vector2 position1, Vector2 position2, float radius1, float radius2)
+// mtv resolves 1 from 2
+bool CircleCircle(Vector2 position1, Vector2 position2, float radius1, float radius2, Vector2* mtv = nullptr)
 {
-    return Distance(position1, position2) <= radius1 + radius2;
+    float distance = Distance(position1, position2);
+    float radiiSum = radius1 + radius2;
+    bool collision = distance <= radiiSum;
+    if (collision && mtv != nullptr)
+    {
+        *mtv = Normalize(position1 - position2) * (radiiSum - distance);
+    }
+    return collision;
 }
 
 int main(void)
@@ -15,21 +23,24 @@ int main(void)
 
     const Vector2 center{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
     const float radius = 50.0f;
-
+    Vector2 position = center;
     while (!WindowShouldClose())
     {
         const Vector2 cursor = GetMousePosition();
 
-        const bool collision = CircleCircle(cursor, center, radius, radius);
-        const Color color = collision ? RED : GREEN;
+        Vector2 mtv;
+        const bool collision = CircleCircle(position, cursor, radius, radius, &mtv);
+        if (collision)
+            position = position + mtv;
+        const Color color = CircleCircle(position, cursor, radius, radius) ? RED : GREEN;
 
         const float radiiSum = radius + radius;
-        const float distance = Distance(cursor, center);
+        const float distance = Distance(position, cursor);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawCircleV(center, radius, color);
+        DrawCircleV(position, radius, color);
         DrawCircleV(cursor, radius, color);
 
         const float thickness = 5.0f;
