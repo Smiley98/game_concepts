09a839f23a22ed7ec54035a3684b633024108cf7
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-04 15:44:54 -0500

    Removed dead code

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,11 +3,6 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-bool CircleCircle(Vector2 position1, Vector2 position2, float radius1, float radius2)
-{
-    return Distance(position1, position2) <= radius1 + radius2;
-}
-
 bool CircleRect(Vector2 circleCenter, float radius, Vector2 rectCenter, Vector2 halfExtents)
 {
     Vector2 nearest;
@@ -18,6 +13,7 @@ bool CircleRect(Vector2 circleCenter, float radius, Vector2 rectCenter, Vector2 
     nearest.x = fabsf(circleCenter.x - xMin) < fabsf(circleCenter.x - xMax) ? xMin : xMax;
     nearest.y = fabsf(circleCenter.y - yMin) < fabsf(circleCenter.y - yMax) ? yMin : yMax;
     return Distance(circleCenter, nearest) <= radius;
+    // Optimization -- return DistanceSqr(circleCenter, nearest) <= radius * radius;
 }
 
 int main(void)
@@ -30,33 +26,18 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-        const Vector2 mouse = GetMousePosition();
         const float rw = 60.0f;
         const float rh = 40.0f;
-
+        const Vector2 mouse = GetMousePosition();
         Color color = CircleRect(center, radius, mouse, { rw * 0.5f, rh * 0.5f }) ? RED : GREEN;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
         DrawCircleV(center, radius, color);
-
-        Rectangle r;
-        r.x = mouse.x - rw * 0.5f;
-        r.y = mouse.y - rh * 0.5f;
-        r.width = rw;
-        r.height = rh;
-        DrawRectangleRec(r, color);
-
+        DrawRectangleV({ mouse.x - rw * 0.5f, mouse.y - rh * 0.5f }, { rw, rh }, color);
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
-
-// Optimization -- square distance and radii to remove square-root from calculation
-//bool CircleCircle(Vector2 position1, Vector2 position2, float radius1, float radius2)
-//{
-//    return DistanceSqr(position1, position2) <= powf((radius1 + radius2), 2.0f);
-//}
