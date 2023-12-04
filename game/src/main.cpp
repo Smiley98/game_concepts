19654e093ba17596c8698299141a48d04f40b699
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-04 15:42:29 -0500

    Response

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -3,12 +3,8 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-bool CircleCircle(Vector2 position1, Vector2 position2, float radius1, float radius2)
-{
-    return Distance(position1, position2) <= radius1 + radius2;
-}
-
-bool CircleRect(Vector2 circleCenter, float radius, Vector2 rectCenter, Vector2 halfExtents)
+// Rectangle defined by center position + half-extents. Mtv points FROM rectangle TO circle
+bool CircleRect(Vector2 circleCenter, float radius, Vector2 rectCenter, Vector2 halfExtents, Vector2* mtv = nullptr)
 {
     Vector2 nearest;
     float xMin = rectCenter.x - halfExtents.x;
@@ -17,7 +13,12 @@ bool CircleRect(Vector2 circleCenter, float radius, Vector2 rectCenter, Vector2 
     float yMax = rectCenter.y + halfExtents.y;
     nearest.x = fabsf(circleCenter.x - xMin) < fabsf(circleCenter.x - xMax) ? xMin : xMax;
     nearest.y = fabsf(circleCenter.y - yMin) < fabsf(circleCenter.y - yMax) ? yMin : yMax;
-    return Distance(circleCenter, nearest) <= radius;
+
+    float distance = Distance(circleCenter, nearest);
+    bool collision = distance <= radius;
+    if (collision && mtv != nullptr)
+        *mtv = Normalize(circleCenter - rectCenter) * (radius - distance);
+    return collision;
 }
 
 int main(void)
@@ -25,38 +26,28 @@ int main(void)
     InitWindow(1280, 720, "Game");
     SetTargetFPS(60);
 
-    const Vector2 center{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
-    const float radius = 50.0f;
+    Vector2 circlePosition{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+    float radius = 50.0f;
 
     while (!WindowShouldClose())
     {
-        const Vector2 mouse = GetMousePosition();
-        const float rw = 60.0f;
-        const float rh = 40.0f;
+        Vector2 mouse = GetMousePosition();
+        float rw = 60.0f;
+        float rh = 40.0f;
 
-        Color color = CircleRect(center, radius, mouse, { rw * 0.5f, rh * 0.5f }) ? RED : GREEN;
+        Vector2 mtv;
+        bool collision = CircleRect(circlePosition, radius, mouse, { rw * 0.5f, rh * 0.5f }, &mtv);
+        Color color = collision ? RED : GREEN;
+        if (collision)
+            circlePosition = circlePosition + mtv;
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        DrawCircleV(center, radius, color);
-
-        Rectangle r;
-        r.x = mouse.x - rw * 0.5f;
-        r.y = mouse.y - rh * 0.5f;
-        r.width = rw;
-        r.height = rh;
-        DrawRectangleRec(r, color);
-
+        DrawCircleV(circlePosition, radius, color);
+        DrawRectangleV({ mouse.x - rw * 0.5f , mouse.y - rh * 0.5f }, { rw, rh }, color);
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
-
-// Optimization -- square distance and radii to remove square-root from calculation
-//bool CircleCircle(Vector2 position1, Vector2 position2, float radius1, float radius2)
-//{
-//    return DistanceSqr(position1, position2) <= powf((radius1 + radius2), 2.0f);
-//}
