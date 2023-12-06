19199917593c39fa772b7ebca43b7f1998c5dc7e
Smiley98 <smileysofficialcinema@gmail.com>
2023-12-06 10:37:53 -0500

    Added circle-rectangle visualization

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -29,7 +29,33 @@ int main(void)
         const float rw = 60.0f;
         const float rh = 40.0f;
         const Vector2 mouse = GetMousePosition();
+        const Vector2 circleCenter = center;
+        const Vector2 rectCenter = mouse;
         Color color = CircleRect(center, radius, mouse, { rw * 0.5f, rh * 0.5f }) ? RED : GREEN;
+
+
+        Vector2 nearest;
+        float xMin = rectCenter.x - rw * 0.5f;
+        float xMax = rectCenter.x + rw * 0.5f;
+        float yMin = rectCenter.y - rh * 0.5f;
+        float yMax = rectCenter.y + rh * 0.5f;
+
+        float dxMin = fabsf(circleCenter.x - xMin);
+        float dxMax = fabsf(circleCenter.x - xMax);
+        float dyMin = fabsf(circleCenter.y - yMin);
+        float dyMax = fabsf(circleCenter.y - yMax);
+
+        nearest.x = fabsf(circleCenter.x - xMin) < fabsf(circleCenter.x - xMax) ? xMin : xMax;
+        nearest.y = fabsf(circleCenter.y - yMin) < fabsf(circleCenter.y - yMax) ? yMin : yMax;
+
+        Vector2 start{ 100.0f, 100.0f };
+        DrawLineEx(start, { start.x + dxMin, start.y }, 5.0f, RED);
+        start.y += 5.0f;
+        DrawLineEx(start, { start.x + dxMax, start.y }, 5.0f, ORANGE);
+        start.y += 5.0f;
+        DrawLineEx(start, { start.x + dyMin, start.y }, 5.0f, GREEN);
+        start.y += 5.0f;
+        DrawLineEx(start, { start.x + dyMax, start.y }, 5.0f, BLUE);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
